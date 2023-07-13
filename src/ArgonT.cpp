60dbc15474bf18c34e068ae559f1749d3dd58887
DoBeJamin <benjaminick11@gmail.com>
2023-07-12 23:50:55 -0500

    working I think

--- src/ArgonT.cpp
+++ src/ArgonT.cpp
@@ -4,12 +4,25 @@
 
 #include "Particle.h"
 #line 1 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Capstone/ArgonT/src/ArgonT.ino"
+#include "MQTT.h"
+#include "LIS3DH.h"
+
+void callback(char *topic, byte *payload, unsigned int length);
 void setup();
 void loop();
-#line 1 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Capstone/ArgonT/src/ArgonT.ino"
+#line 4 "c:/Users/benja/OneDrive/Desktop/visualStudioCode/summerIOT/Capstone/ArgonT/src/ArgonT.ino"
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(MANUAL);
 
+
+//MQTT
+void callback(char *topic, byte *payload, unsigned int length) {};
+MQTT client("lab.thewcl.com", 1883, callback);
+
+//accelerometer
+LIS3DHSPI accel(SPI, D3, WKP);
+
+//Bluetoth Beacon
 SerialLogHandler logHandler;
 
 void connectedCallback(const BlePeerDevice& peer, void* context);
@@ -18,8 +31,14 @@ void disconnectedCallback(const BlePeerDevice& peer, void* context);
 BleUuid serviceUuid("afe7acc5-33a9-478f-bbe1-8944aa08e884");
 
 void setup() {
-	BLE.on();
 
+	//configure accelerometer
+	LIS3DHConfig config;
+	config.setAccelMode(LIS3DH::RATE_100_HZ);
+	accel.setup(config);
+
+	//bluetooth
+	BLE.on();
 	BLE.onConnected(connectedCallback, NULL);
 	BLE.onDisconnected(disconnectedCallback, NULL);
 
@@ -31,6 +50,23 @@ void setup() {
 
 void loop() {
 
+	LIS3DHSample sample;
+  	if (accel.getSample(sample)) {
+
+		int x = sample.x;
+		int y = sample.y;
+		int z = sample.z;
+
+		String xs = String(x);
+		String ys = String(y);
+		String zs = String(z);
+
+
+		client.publish("BAR/argonT/accel/x", xs);
+		client.publish("BAR/argonT/accel/y", ys);
+		client.publish("BAR/argonT/accel/z", zs);
+
+	}
 }
 
 
--- src/ArgonT.ino
+++ src/ArgonT.ino
@@ -1,6 +1,18 @@
+#include "MQTT.h"
+#include "LIS3DH.h"
+
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(MANUAL);
 
+
+//MQTT
+void callback(char *topic, byte *payload, unsigned int length) {};
+MQTT client("lab.thewcl.com", 1883, callback);
+
+//accelerometer
+LIS3DHSPI accel(SPI, D3, WKP);
+
+//Bluetoth Beacon
 SerialLogHandler logHandler;
 
 void connectedCallback(const BlePeerDevice& peer, void* context);
@@ -9,8 +21,14 @@ void disconnectedCallback(const BlePeerDevice& peer, void* context);
 BleUuid serviceUuid("afe7acc5-33a9-478f-bbe1-8944aa08e884");
 
 void setup() {
-	BLE.on();
 
+	//configure accelerometer
+	LIS3DHConfig config;
+	config.setAccelMode(LIS3DH::RATE_100_HZ);
+	accel.setup(config);
+
+	//bluetooth
+	BLE.on();
 	BLE.onConnected(connectedCallback, NULL);
 	BLE.onDisconnected(disconnectedCallback, NULL);
 
@@ -22,6 +40,24 @@ void setup() {
 
 void loop() {
 
+	LIS3DHSample sample;
+  	if (accel.getSample(sample)) {
+
+		//some tomfoolery bc publish didn't like taking in an int and sample didn't like being put into a String
+		int x = sample.x;
+		int y = sample.y;
+		int z = sample.z;
+
+		String xs = String(x);
+		String ys = String(y);
+		String zs = String(z);
+
+
+		client.publish("BAR/argonT/accel/x", xs);
+		client.publish("BAR/argonT/accel/y", ys);
+		client.publish("BAR/argonT/accel/z", zs);
+
+	}
 }
 
 
